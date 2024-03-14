34d4590bf6ad872b3d423d4686994e0cd7699e97
sunjianrong <sunruolifeng@gmail.com>
2024-03-14 21:29:34 +0800

    feat: 引入树形菜单分类结构

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-tree :data="data" @node-click="handleNodeClick"></el-tree>
+  </div>
+</template>
+<script>
+export default {
+  name: 'category',
+  data () {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list'),
+        method: 'get'
+      }).then(data => {
+        console.log('成功获取到菜单数据')
+      })
+    }
+  },
+  created () {
+    this.getMenus()
+  }
+}
+</script>
+<style scoped lang="scss">
+
+</style>
