f390c6b52dd9801c510ccf084dfcc6fdbd791e20
sunjianrong <sunruolifeng@gmail.com>
2024-03-15 08:51:11 +0800

    feat: 显示三级分类数据

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-tree :data="data" @node-click="handleNodeClick"></el-tree>
+    <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
   </div>
 </template>
 <script>
@@ -8,10 +8,10 @@ export default {
   name: 'category',
   data () {
     return {
-      data: [],
+      menus: [],
       defaultProps: {
         children: 'children',
-        label: 'label'
+        label: 'name'
       }
     }
   },
@@ -23,8 +23,9 @@ export default {
       this.$http({
         url: this.$http.adornUrl('/product/category/list'),
         method: 'get'
-      }).then(data => {
-        console.log('成功获取到菜单数据')
+      }).then(({data}) => {
+        console.log(data.data)
+        this.menus = data.data
       })
     }
   },
